7900dde51370deda34be8443791a0a28baa03bd1
lhcoyle4 <lhcoyle4@gmail.com>
2015-10-20 18:38:51 -0400

    tried to compile, cleaned up easy errors

--- minres.c
+++ minres.c
@@ -1,19 +1,19 @@
 #include <stdio.h>  /* printf */
 #include <stdlib.h> /* atoi */
 
-//having a problem right now with which data structures to pass around
-//I don't think that multi-D arrays will work quite right.
-//it might need to be arrays of structs which contain flags
+/*having a problem right now with which data structures to pass around
+ *I don't think that multi-D arrays will work quite right.
+ *it might need to be arrays of structs which contain flags*/
 
-//another problem is that my resistor values are going to need to be floats
-//i'll update that later but right now i'm just trying to learn git
+/*another problem is that my resistor values are going to need to be floats
+ *i'll update that later but right now i'm just trying to learn git*/
 
 void PrintHelp(void)
 {
-  //print the correct usage
+  /*print the correct usage*/
   printf("Usage:\n\t minres <value of resistor> <number of resistors> <desired REQ>\n");
   
-  //and an example usage
+  /*and an example usage*/
   printf("Example:\n\t minres 100 100 99\n");
 }
 
@@ -29,7 +29,7 @@ int** GenCombos(int resval, int numres, int reqval)
   
 }
 
-//helper function for EquivRes
+/*helper function for EquivRes*/
 int Parallel(int* parallelres, int size)
 {
   int i = 0;
@@ -46,8 +46,8 @@ int Parallel(int* parallelres, int size)
   return reqparallel;
 }
 
-//calculates the equivalent resistances for all of the combinations, and returns
-//them in an array.
+/*calculates the equivalent resistances for all of the combinations, and returns*/
+/*them in an array.*/
 int* EquivRes(int** combos)
 {
   
@@ -55,14 +55,14 @@ int* EquivRes(int** combos)
 
 void PrintMinRes(int* minres)
 {
-  //lots of printf statements, to print what the circuit would look like on cmd line
-  /*
+  /*lots of printf statements, to print what the circuit would look like on cmd line
+   *
    *          |---100---|
    * ---100---|         |---100--- 
    *          |---100---|
    *          
-   */
-  //something like above
+   * 
+   *something like above*/
 }
 
 int main(int argc, char *argv[])
@@ -72,28 +72,28 @@ int main(int argc, char *argv[])
   int reqval = 0;
   int* minres = 0;
   
-  //check for appropriate usage of the program
+  /*check for appropriate usage of the program*/
   if(argc < 2)
     PrintHelp();
   
-  //default to an arbitrarily large amount of resistors if there is no 4th arg
+  /*default to an arbitrarily large amount of resistors if there is no 4th arg*/
   if (argc == 3)
     numres = 1000;
   
-  //parse the command line for the value of the resistor to be used,
+  /*parse the command line for the value of the resistor to be used,*/
   resval = atoi(argv[1]);
   
-  //the number of resistors available
+  /*the number of resistors available*/
   numres = atoi(argv[2]);
   
-  //the value of the desired equivalent resistances
+  /*the value of the desired equivalent resistances*/
   reqval = atoi(argv[3]);
   
-  //use the values to generate list of all combinations of resistors
-  //and calculate the equivalent resistances for all entries in list
+  /*use the values to generate list of all combinations of resistors
+   *and calculate the equivalent resistances for all entries in list*/
   minres = EquivRes(GenCombos(resval, numres, reqval));
   
-  //return the entry which has the fewest resistors in a readable format
+  /*return the entry which has the fewest resistors in a readable format*/
   PrintMinRes(minres);
   
   return 0;
